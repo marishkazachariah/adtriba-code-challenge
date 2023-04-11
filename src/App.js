7062a7b3f31ccfcca22c06b81682de696b9a6fb0
Marishka Zachariah <marishka.zachariah@gmail.com>
2023-04-11 20:30:01 +0200

    init project setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SphereSampleData from "./components/SphereSampleData";
+function App() {
+  return (
+    <div className="app">
+      <SphereSampleData />
+    </div>
+  );
+}
+
+export default App;
